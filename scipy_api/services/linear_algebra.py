d32f86c35e62ad802ed515636316deffaf75fccc
alexdo21 <alex.do21@yahoo.com>
2022-06-29 02:40:38 -0500

    formatting change

--- scipy_api/services/linear_algebra.py
+++ scipy_api/services/linear_algebra.py
@@ -2,9 +2,9 @@ import numpy as np
 from scipy import linalg
 
 
-def inverse(A: np.ndarray) -> np.ndarray:
-    return linalg.inv(A)
-
-
 def determinant(A: np.ndarray) -> float:
     return linalg.det(A)
+
+
+def inverse(A: np.ndarray) -> np.ndarray:
+    return linalg.inv(A)
